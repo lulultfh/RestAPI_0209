b2f37599cfa95b2b115327173cdcd8279ddfdf62
lulultfh <punyalulu05@gmail.com>
2026-05-01 19:06:55 +0700

    feat: menambahkan hewan event

---
+++ lib/logic/bloc/hewan/hewan_event.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+
+abstract class HewanEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class FetchHewan extends HewanEvent {}
+
+class CreateHewan extends HewanEvent {
+  final Map<String, dynamic> data;
+  CreateHewan(this.data);
+  @override
+  List<Object> get props => [data];
+}
+
+class UpdateHewan extends HewanEvent {
+  final int id;
+  final Map<String, dynamic> data;
+  UpdateHewan(this.id, this.data);
+  @override
+  List<Object> get props => [data];
+}
+
+class DeleteHewan extends HewanEvent {
+  final int id;
+  DeleteHewan(this.id);
+  @override
+  List<Object> get props => [id];
+}
