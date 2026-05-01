21675a03cc3ba9c9c769b7dbc4be4f676a6d564e
lulultfh <punyalulu05@gmail.com>
2026-05-01 17:43:13 +0700

    feat: menambahkan class HewanModel dengan variabel id, nama, jenis, tanggalLahir, harga, dan status, serta menambahkan constructor dan methos formJson untuk handle mappping API

---
+++ lib/data/models/hewan_models.dart
@@ -0,0 +1,28 @@
+class HewanModel {
+  final int id;
+  final String nama;
+  final String jenis;
+  final String tanggalLahir;
+  final int harga;
+  final String status;
+
+  HewanModel({
+    required this.id,
+    required this.nama,
+    required this.jenis,
+    required this.tanggalLahir,
+    required this.harga,
+    required this.status,
+  });
+
+  factory HewanModel.fromJson(Map<String, dynamic> json) {
+    return HewanModel(
+      id: json['id'],
+      nama: json['nama'],
+      jenis: json['jenis'],
+      tanggalLahir: json['tanggal_lahir'],
+      harga: json['harga'],
+      status: json['status'],
+    );
+  }
+}
