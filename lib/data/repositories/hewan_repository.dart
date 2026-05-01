22da553aa6c9b07b4d13f67271c2e52d7bedd7d7
lulultfh <punyalulu05@gmail.com>
2026-05-01 18:30:47 +0700

    feat: menambahkan HewanRepository untuk handling crud hewan dan mengintegrasikan API serta authentication token

---
+++ lib/data/repositories/hewan_repository.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:restapi_flutter/data/models/hewan_models.dart';
+import 'package:restapi_flutter/data/providers/storage_provider.dart';
+import 'package:http/http.dart' as http;
+
+class HewanRepository {
+  final String baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
+  final StorageProvider storage = StorageProvider();
+
+  Future<List<HewanModel>> getAllHewan() async {
+    final token = await storage.getToken();
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/hewan'),
+      headers: {'Authorization': 'Bearer $token', 'Accept': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> body = jsonDecode(response.body);
+      final List<dynamic> data = body['data'];
+      return data.map((item) => HewanModel.fromJson(item)).toList();
+    } else {
+      throw Exception("Gagal mengambil data hewan");
+    }
+  }
+
+  Future<void> createHewan(Map<String, dynamic> hewanData) async {
+    final token = await storage.getToken();
+
+    final response = await http.post(
+      Uri.parse('$baseUrl/hewan'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(hewanData),
+    );
+    if (response.statusCode != 201 && response.statusCode != 200) {
+      final data = jsonDecode(response.body);
+      throw data['message'] ?? 'Gagal menambahkan hewan';
+    }
+  }
+
+  Future<void> updateHewan(int id, Map<String, dynamic> hewanData) async {
+    final token = await storage.getToken();
+    final response = await http.put(
+      Uri.parse('$baseUrl/hewan/$id'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(hewanData),
+    );
+    if (response.statusCode != 200) {
+      final data = jsonDecode(response.body);
+      throw data['message'] ?? 'Gagal memperbarui data hewan';
+    }
+  }
+
+  Future<void> deleteHewan(int id) async {
+    final token = await storage.getToken();
+    final response = await http.delete(
+      Uri.parse('$baseUrl/hewan/$id'),
+      headers: {'Accept': 'application/json', 'Authorization': 'Bearer $token'},
+    );
+    if (response.statusCode != 200 && response.statusCode != 204) {
+      final data = jsonDecode(response.body);
+      throw data['message'] ?? 'Gagal menghapus data hewan';
+    }
+  }
+}
