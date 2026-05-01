1d31e847aa9f4a9defa6eefcc1f4a55af09df207
lulultfh <punyalulu05@gmail.com>
2026-05-01 19:26:19 +0700

    feat: menambahkan hewan bloc

---
+++ lib/logic/bloc/hewan/hewan_bloc.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restapi_flutter/data/repositories/hewan_repository.dart';
+import 'package:restapi_flutter/logic/bloc/hewan/hewan_event.dart';
+import 'package:restapi_flutter/logic/bloc/hewan/hewan_state.dart';
+
+class HewanBloc extends Bloc<HewanEvent, HewanState> {
+  final HewanRepository repository;
+
+  HewanBloc({required this.repository}) : super(HewanInitial()) {
+    on<FetchHewan>((event, emit) async {
+      emit(HewanLoading());
+      try {
+        final list = await repository.getAllHewan();
+        emit(HewanLoaded(list));
+      } catch (e) {
+        emit(HewanError(e.toString()));
+      }
+    });
+    on<CreateHewan>((event, emit) async {
+      emit(HewanLoading());
+      try {
+        await repository.createHewan(event.data);
+        emit(HewanCreatedSuccess());
+        add(FetchHewan());
+      } catch (e) {
+        emit(HewanError(e.toString()));
+      }
+    });
+    on<UpdateHewan>((event, emit) async {
+      emit(HewanLoading());
+      try {
+        await repository.updateHewan(event.id, event.data);
+        emit(HewanCreatedSuccess());
+        add(FetchHewan());
+      } catch (e) {
+        emit(HewanError(e.toString()));
+      }
+    });
+    on<DeleteHewan>((event, emit) async {
+      emit(HewanLoading());
+      try {
+        await repository.deleteHewan(event.id);
+        emit(HewanCreatedSuccess());
+        add(FetchHewan());
+      } catch (e) {
+        emit(HewanError(e.toString()));
+      }
+    });
+  }
+}
