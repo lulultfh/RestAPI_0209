c8692ae284d2a848fd175eda591d48217dd6fff0
lulultfh <punyalulu05@gmail.com>
2026-05-02 16:37:52 +0700

    feat: menambahkan parameter initialData, onSubmit, dan isLoading pada HewanFormWidget

--- lib/ui/widgets/hewan_form_widget.dart
+++ lib/ui/widgets/hewan_form_widget.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:restapi_flutter/data/models/hewan_models.dart';
 
 class HewanFormWidget extends StatefulWidget {
-  const HewanFormWidget({super.key});
+  final HewanModel? intialData;
+  final void Function(Map<String, dynamic> data) onSubmit;
+  final bool isLoading;
+
+  const HewanFormWidget({
+    super.key,
+    this.intialData,
+    required this.onSubmit,
+    this.isLoading = false,
+  });
 
   @override
   State<HewanFormWidget> createState() => _HewanFormWidgetState();
