3f9d94ded59c966b78bee1054f5d210af41371ac
lulultfh <punyalulu05@gmail.com>
2026-05-02 17:24:25 +0700

    refactor: update class editHewanPage sebagai turunan dari StatelessWidget

---
+++ lib/ui/pages/edit_hewan_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditHewanPage extends StatelessWidget {
+  const EditHewanPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
