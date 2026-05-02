d309e20b4cb1fbf8292646e8b496c549739b1483
lulultfh <punyalulu05@gmail.com>
2026-05-02 16:06:27 +0700

    feat: menambahkan method dispose

--- lib/ui/pages/add_hewan_page.dart
+++ lib/ui/pages/add_hewan_page.dart
@@ -15,6 +15,17 @@ class _AddHewanPageState extends State<AddHewanPage> {
   final _tglLahirController = TextEditingController();
   final _hargaController = TextEditingController();
   final _statusController = TextEditingController();
+
+  @override
+  void dispose() {
+    _namaController.dispose();
+    _jenisController.dispose();
+    _tglLahirController.dispose();
+    _hargaController.dispose();
+    _statusController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
