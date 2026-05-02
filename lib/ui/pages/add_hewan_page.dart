cee77ea916a0224daea607632d10a478d4414db8
lulultfh <punyalulu05@gmail.com>
2026-05-02 17:17:31 +0700

    refactor: update AddHewanPage class sebagai turunan dari StatelessWidget

--- lib/ui/pages/add_hewan_page.dart
+++ lib/ui/pages/add_hewan_page.dart
@@ -1,30 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AddHewanPage extends StatefulWidget {
+class AddHewanPage extends StatelessWidget {
   const AddHewanPage({super.key});
-
-  @override
-  State<AddHewanPage> createState() => _AddHewanPageState();
-}
-
-class _AddHewanPageState extends State<AddHewanPage> {
-  final _formKey = GlobalKey<FormState>();
-
-  final _namaController = TextEditingController();
-  final _jenisController = TextEditingController();
-  final _tglLahirController = TextEditingController();
-  final _hargaController = TextEditingController();
-  final _statusController = TextEditingController();
-
-  @override
-  void dispose() {
-    _namaController.dispose();
-    _jenisController.dispose();
-    _tglLahirController.dispose();
-    _hargaController.dispose();
-    _statusController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
