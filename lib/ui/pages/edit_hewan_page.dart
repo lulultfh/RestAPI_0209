064051d1a89eec11b1026bc0fc9487b6e4cd0ef4
lulultfh <punyalulu05@gmail.com>
2026-05-02 17:39:15 +0700

    feat: menambahkan view untuk halaman edit

--- lib/ui/pages/edit_hewan_page.dart
+++ lib/ui/pages/edit_hewan_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restapi_flutter/data/models/hewan_models.dart';
+import 'package:restapi_flutter/logic/bloc/hewan/hewan_bloc.dart';
+import 'package:restapi_flutter/logic/bloc/hewan/hewan_event.dart';
+import 'package:restapi_flutter/logic/bloc/hewan/hewan_state.dart';
+import 'package:restapi_flutter/ui/widgets/hewan_form_widget.dart';
 
 class EditHewanPage extends StatelessWidget {
   final HewanModel hewan;
@@ -8,6 +13,29 @@ class EditHewanPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Edit Hewan')),
+      body: BlocConsumer<HewanBloc, HewanState>(
+        listener: (context, state) {
+          if (state is HewanCreatedSuccess) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Data hewan berhasil diupdate')),
+            );
+          } else if (state is HewanError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Gagal: ${state.message}')),
+            );
+          }
+        },
+        builder: (context, state){
+          final isLoading = state is HewanLoading;
+          return HewanFormWidget(
+            isLoading: isLoading,
+            onSubmit: (data){
+              context.read<HewanBloc>().add(UpdateHewan(hewan.id, data));
+            });
+        },
+      ),
+    );
   }
 }
