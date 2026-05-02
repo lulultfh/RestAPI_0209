33c82d491fbec2d790d2aadddd0b5393f78019c4
lulultfh <punyalulu05@gmail.com>
2026-05-02 16:33:02 +0700

    feat: menambahkan class HewanFormWidget sebagai turunan dari class StatefulWidget

---
+++ lib/ui/widgets/hewan_form_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HewanFormWidget extends StatefulWidget {
+  const HewanFormWidget({super.key});
+
+  @override
+  State<HewanFormWidget> createState() => _HewanFormWidgetState();
+}
+
+class _HewanFormWidgetState extends State<HewanFormWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
