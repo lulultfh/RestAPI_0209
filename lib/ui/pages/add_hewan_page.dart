b615690fc2fa33cfcf6c150980a33b6a36e62831
lulultfh <punyalulu05@gmail.com>
2026-05-02 15:56:01 +0700

    feat: menambahkan class AddHewanPage sebagai turunan dari statefulWidget

---
+++ lib/ui/pages/add_hewan_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddHewanPage extends StatefulWidget {
+  const AddHewanPage({super.key});
+
+  @override
+  State<AddHewanPage> createState() => _AddHewanPageState();
+}
+
+class _AddHewanPageState extends State<AddHewanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
